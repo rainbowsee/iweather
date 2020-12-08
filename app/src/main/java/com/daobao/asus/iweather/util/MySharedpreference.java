119209a400d9d51e335b4648d4953285911c20d3
THUNDER <1520923191@qq.com>
2020-12-08 12:54:58 +0800

    Weather

---
+++ app/src/main/java/com/daobao/asus/iweather/util/MySharedpreference.java
@@ -0,0 +1,17 @@
+package com.daobao.asus.iweather.util;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ */
+
+public class MySharedpreference {
+    public static SharedPreferences preferences;
+    public static SharedPreferences.Editor getInstance(Context context)
+    {
+        preferences = context.getSharedPreferences("Info",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=preferences.edit();
+        return editor;
+    }
+}
